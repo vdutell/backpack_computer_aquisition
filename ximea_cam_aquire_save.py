78c1abf8157be1466912fab1c39870833431d83f
Vasha <vashadutell@gmail.com>
2019-06-19 15:56:16 -0700

    added auto white balance but doensnt seem to do anything. At 197fps'

--- ximea_cam_aquire_save.py
+++ ximea_cam_aquire_save.py
@@ -17,11 +17,13 @@ def print_cam_settings(cam):
     print(f'Framerate is set to {cam.get_framerate()} fps')
     #data format
     print(f'Data format is set to {cam.get_imgdataformat()}')
-    
+    #white balance
+    #print(f'White balance is set to {cam.get_autowb()}')
 
     return()
 
-def apply_cam_settings(cam, timing_mode=None, exposure=None, framerate=None, gain=None, format=None):
+def apply_cam_settings(cam, timing_mode=None, exposure=None, framerate=None,
+                        gain=None, img_format=None, auto_wb=None):
     """
     Apply settings to the camera
     """
@@ -39,93 +41,20 @@ def apply_cam_settings(cam, timing_mode=None, exposure=None, framerate=None, gai
     if(gain):
         cam.set_gain(gain)
     #Format:
-    if(format):
-        cam.set_imgdataformat(format)
+    if(img_format):
+        cam.set_imgdataformat(img_format)
+    #White Blalance
+    if(auto_wb):
+        cam.set_param('auto_wb', 1)
+        #cam.set_auto_wb(auto_wb)
+
 
     return(cam)
-
-def aquire_save_filewise(num_frames=10, cam_timing_mode='XI_ACQ_TIMING_MODE_FREE_RUN', 
-                         cam_exposure=500, 
-                         cam_framerate=1000, cam_gain=100, img_format='XI_RAW8',
-                         verbose=True):
-    """
-    Aquire a set of files and save them each as they are collected
-    """
-    #create instance for first connected camera
-    cam = xiapi.Camera()
-
-    #start communication
-    #to open specific device, use:
-    #cam.open_device_by_SN('41305651')
-    #(open by serial number)
-    if(verbose):
-        print('Opening first camera...')
-    cam.open_device()
-
-    try:
-
-        #apply settings to our camera device
-        cam = apply_cam_settings(cam, timing_mode=cam_timing_mode, exposure=cam_exposure, 
-                                framerate=cam_framerate, gain=cam_gain, format=img_format)
-        if(verbose):
-            print_cam_settings(cam)
-
-        #create instance of Image to store image data and metadata
-        img = xiapi.Image()
-
-        #start data acquisition
-        if(verbose):
-            print(f'Starting data acquisition of {num_frames} frames...',end='')
-        cam.start_acquisition()
-
-        #time start
-        tic = time.clock()
-        for i in range(num_frames):
-            if(verbose):
-                print('*',end='')
-            #get data and pass them from camera to img
-            cam.get_image(img)
-
-            #get raw data from camera
-            #for Python2.x function returns string
-            #for Python3.x function returns bytes
-            data_raw = img.get_image_data_raw()
-
-
-            #write to bitearray
-            with open(f'capture/data_raw_bytes_{i}.bin', 'wb') as f:
-                f.write(data_raw)  
-
-        #time stop
-        toc = time.clock()
-        if(verbose):
-            print(f'\nEffective Speed: {num_frames/(toc-tic):.2f} FPS')
-
-        #stop data acquisition
-        if(verbose):
-            print('Stopping acquisition...')
-        cam.stop_acquisition()
-
-        #stop communication
-        cam.close_device()
-
-        if(verbose):
-            print('Done.')
-        
-        return()
-
-    except Exception as e:
-        print('Something Failed. Cleanly closing the device.')
-        #stop communication
-        cam.close_device()
-
-        return(e)
-
-
 
 def aquire_save_fullset(num_frames=10, cam_timing_mode='XI_ACQ_TIMING_MODE_FREE_RUN', 
                         cam_exposure=500, 
                          cam_framerate=1000, cam_gain=1, img_format='XI_RAW8',
+                         img_autowb=1,
                          verbose=True):
     """
     Aquire a set of files and only save after they have all been collected
@@ -145,7 +74,8 @@ def aquire_save_fullset(num_frames=10, cam_timing_mode='XI_ACQ_TIMING_MODE_FREE_
 
         #apply settings to our camera device
         cam = apply_cam_settings(cam, timing_mode=cam_timing_mode, exposure=cam_exposure, 
-                                framerate=cam_framerate, gain=cam_gain, format=img_format)
+                                framerate=cam_framerate, gain=cam_gain, img_format=img_format,
+                                auto_wb=img_autowb)
         if(verbose):
             print_cam_settings(cam)
 
